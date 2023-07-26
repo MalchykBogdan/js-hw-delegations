800c864118b19c1e34f9693a7d53bd2fa7668fbe
MalchykBogdan <124812110+MalchykBogdan@users.noreply.github.com>
2023-07-26 14:48:11 +0200

    Add files via upload

---
+++ app.js
@@ -0,0 +1,128 @@
+const galleryItems = [
+  {
+    preview:
+      'https://cdn.pixabay.com/photo/2019/05/14/16/43/himilayan-blue-poppy-4202825__340.jpg',
+    original:
+      'https://cdn.pixabay.com/photo/2019/05/14/16/43/himilayan-blue-poppy-4202825_1280.jpg',
+    description: 'Hokkaido Flower',
+  },
+  {
+    preview:
+      'https://cdn.pixabay.com/photo/2019/05/14/22/05/container-4203677__340.jpg',
+    original:
+      'https://cdn.pixabay.com/photo/2019/05/14/22/05/container-4203677_1280.jpg',
+    description: 'Container Haulage Freight',
+  },
+  {
+    preview:
+      'https://cdn.pixabay.com/photo/2019/05/16/09/47/beach-4206785__340.jpg',
+    original:
+      'https://cdn.pixabay.com/photo/2019/05/16/09/47/beach-4206785_1280.jpg',
+    description: 'Aerial Beach View',
+  },
+  {
+    preview:
+      'https://cdn.pixabay.com/photo/2016/11/18/16/19/flowers-1835619__340.jpg',
+    original:
+      'https://cdn.pixabay.com/photo/2016/11/18/16/19/flowers-1835619_1280.jpg',
+    description: 'Flower Blooms',
+  },
+  {
+    preview:
+      'https://cdn.pixabay.com/photo/2018/09/13/10/36/mountains-3674334__340.jpg',
+    original:
+      'https://cdn.pixabay.com/photo/2018/09/13/10/36/mountains-3674334_1280.jpg',
+    description: 'Alpine Mountains',
+  },
+  {
+    preview:
+      'https://cdn.pixabay.com/photo/2019/05/16/23/04/landscape-4208571__340.jpg',
+    original:
+      'https://cdn.pixabay.com/photo/2019/05/16/23/04/landscape-4208571_1280.jpg',
+    description: 'Mountain Lake Sailing',
+  },
+  {
+    preview:
+      'https://cdn.pixabay.com/photo/2019/05/17/09/27/the-alps-4209272__340.jpg',
+    original:
+      'https://cdn.pixabay.com/photo/2019/05/17/09/27/the-alps-4209272_1280.jpg',
+    description: 'Alpine Spring Meadows',
+  },
+  {
+    preview:
+      'https://cdn.pixabay.com/photo/2019/05/16/21/10/landscape-4208255__340.jpg',
+    original:
+      'https://cdn.pixabay.com/photo/2019/05/16/21/10/landscape-4208255_1280.jpg',
+    description: 'Nature Landscape',
+  },
+  {
+    preview:
+      'https://cdn.pixabay.com/photo/2019/05/17/04/35/lighthouse-4208843__340.jpg',
+    original:
+      'https://cdn.pixabay.com/photo/2019/05/17/04/35/lighthouse-4208843_1280.jpg',
+    description: 'Lighthouse Coast Sea',
+  },
+];
+
+function createGalleryItem(item) {
+  const listItem = document.createElement('li');
+  listItem.classList.add('gallery__item');
+
+  const link = document.createElement('a');
+  link.classList.add('gallery__link');
+  link.href = item.original;
+
+  const image = document.createElement('img');
+  image.classList.add('gallery__image');
+  image.src = item.preview;
+  image.alt = item.description;
+  image.dataset.source = item.original;
+
+  link.appendChild(image);
+  listItem.appendChild(link);
+
+  return listItem;
+}
+
+const galleryList = document.querySelector('.js-gallery');
+const galleryFragment = document.createDocumentFragment();
+
+galleryItems.forEach(item => {
+  const galleryItem = createGalleryItem(item);
+  galleryFragment.appendChild(galleryItem);
+});
+
+galleryList.appendChild(galleryFragment);
+
+galleryList.addEventListener('click', event => {
+  event.preventDefault();
+  const target = event.target;
+
+  if (target.nodeName === 'IMG') {
+    const originalImageURL = target.dataset.source;
+    openModal(originalImageURL);
+  }
+});
+
+function openModal(url) {
+  const lightbox = document.querySelector('.js-lightbox');
+  const lightboxImage = lightbox.querySelector('.lightbox__image');
+
+  lightboxImage.src = url;
+  lightboxImage.alt = galleryItems.find(item => item.original === url).description;
+
+  lightbox.classList.add('is-open');
+}
+
+document.querySelector('[data-action="close-lightbox"]').addEventListener('click', closeModal);
+
+document.querySelector('.lightbox__overlay').addEventListener('click', closeModal);
+
+function closeModal() {
+  const lightbox = document.querySelector('.js-lightbox');
+  const lightboxImage = lightbox.querySelector('.lightbox__image');
+
+  lightboxImage.src = '';
+  lightboxImage.alt = '';
+  lightbox.classList.remove('is-open');
+}
